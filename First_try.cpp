5f15208a9a43574d3a3cc3ca92696d45ceab87c3
Anme <lollox12342345@gmail.com>
2026-02-25 02:03:05 +0300

    second commit width func count

--- First_try.cpp
+++ First_try.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+int count(int a, int b) { return a * b; }
+
 int main() {
   std::cout << "Hello to my house" << std::endl;
+  std::cout << count(2, 3) << std::endl;
   return 0;
 }
