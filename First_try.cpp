6a573432a391c2d2feae5177e6be4e1a3d76fd28
Anme <lollox12342345@gmail.com>
2026-02-25 23:52:14 +0300

    delete block

--- First_try.cpp
+++ First_try.cpp
@@ -4,8 +4,5 @@ int count(int a, int b) { return a * b; }
 
 int main() {
   std::cout << "Hello to my house" << std::endl;
-  std::cout << count(2, 3) << std::endl;
-  std::cout << count(5, 60) << std::endl;
-  std::cout << "pizda" << std::endl;
   return 0;
 }
