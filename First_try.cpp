15b3da7725bbf903ab055326af06996f73889b42
Anme <lollox12342345@gmail.com>
2026-02-25 19:37:53 +0300

    add pizda

--- First_try.cpp
+++ First_try.cpp
@@ -6,5 +6,6 @@ int main() {
   std::cout << "Hello to my house" << std::endl;
   std::cout << count(2, 3) << std::endl;
   std::cout << count(5, 60) << std::endl;
+  std::cout << "pizda" << std::endl;
   return 0;
 }
